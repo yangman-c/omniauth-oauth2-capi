ef1ab52d0ad12f793d2d3e9a12024487413b62bd
mayyang <ym0913@126.com>
2015-11-10 18:14:35 +0800

    add strategy to omniauth with oauth2

---
+++ lib/omniauth/oauth2/capi.rb
@@ -0,0 +1,9 @@
+require "omniauth/oauth2/capi/version"
+
+module Omniauth
+  module Oauth2
+    module Capi
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/omniauth/oauth2/capi/version.rb
@@ -0,0 +1,7 @@
+module Omniauth
+  module Oauth2
+    module Capi
+      VERSION = "0.1.0"
+    end
+  end
+end
